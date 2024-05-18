9b349855e042bbd71e04b44cc0474de618155441
SergeyCherevichenko <cherevichenkosn@gmail.com>
2024-05-18 15:08:33 +0500

    Добавил домашнюю работу к семинару 5

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import ru.cherevichenko.task1.BackupUtility;
+import ru.cherevichenko.task2.TicTacToe;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe.run();
+        BackupUtility.run();
+
+    }
+}
